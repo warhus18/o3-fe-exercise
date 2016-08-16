7292d9796fd44fbdc94a3e0015719179f8a75301
warhusj <warhusj@brolik.com>
2016-08-16 08:52:38 -0400

    responsive exercise

--- responsive/js/scripts.js
+++ responsive/js/scripts.js
@@ -5,4 +5,13 @@ $(function() {
         $(this).toggleClass('active');
     });
 
+    $('.media img').click(function() { 
+	    var article = this;
+	    var src = this.src;
+	    $('.banner img').fadeOut(400,function(){
+	        article.src = this.src;
+	        $(this).fadeIn(400)[0].src = src;
+	    });
+	});
+
 });
