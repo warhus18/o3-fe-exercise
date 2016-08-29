a534cc011b0b1f370006f1d75a96793c3cbfbfc4
warhusj <warhusj@brolik.com>
2016-08-28 22:45:48 -0400

    image swap function

--- responsive/js/scripts.js
+++ responsive/js/scripts.js
@@ -1,8 +1,58 @@
 $(function() {
 
+    // mobile menu
     $('.hamburger').click(function (e) {
         $(this).parent().find('ul').toggleClass('active-menu');
         $(this).toggleClass('active');
     });
 
+    // image swap
+    $('.media').click(function (e) {
+
+        // get the banner image
+        $('.banner').find('picture img').each(function () {
+            bannerImage = $(this).attr('srcset').split('-')[0];
+        });
+
+        // get the media image
+        $(this).find('picture img').each(function () {
+            mediaImage = $(this).attr('srcset').split('-')[0];
+        });
+
+        // swap the media picture
+        $(this).fadeOut(400,function () {
+
+            // replace all image sources
+            $(this).find('source').each(function () {
+                var thisSize = $(this).attr('srcset').split('-')[1]; 
+
+                // set the new srcset
+                $(this).attr('srcset', bannerImage + '-' + thisSize);
+            });
+
+            // change image
+            $(this).find('img').attr('srcset', bannerImage);
+
+            // fade image in
+            $(this).fadeIn(400);
+        });
+
+        // swap the banner picture
+        $('.banner picture').fadeOut(200,function () {
+
+            // replace all image sources
+            $('.banner picture').find('source').each(function () {
+                var thisSize = $(this).attr('srcset').split('-')[1]; 
+
+                // set the new srcset
+                $(this).attr('srcset', mediaImage + '-' + thisSize);
+            });
+
+            // change image
+            $('.banner picture').find('img').attr('srcset', mediaImage);
+
+            // fade image in
+            $('.banner picture').fadeIn(200);
+        });      
+    });
 });
